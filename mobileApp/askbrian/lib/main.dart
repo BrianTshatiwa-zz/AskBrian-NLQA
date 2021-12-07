15ad922f73a740c58e945ff17e458203532fb675
BrianTshatiwa <kutlwano.tshatiwa101@gmail.com>
2021-12-07 21:59:54 +0200

    home page

---
+++ mobileApp/askbrian/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){ 
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      home:DefaultTabController( 
+        length: 2,
+        child: Scaffold(  
+          appBar: AppBar(  
+            title: Text("AskBrian"),
+            backgroundColor: Colors.green[800],
+            bottom: TabBar(tabs: [ 
+              Tab(icon: Icon(Icons.question_answer), text:'Ask'),
+              Tab(icon: Icon(Icons.info), text: 'About',)
+            ],),
+          ),
+          body: TabBarView(children: [ 
+            Text('Question Answer'),
+            Text('About'),
+          ],),
+        ),
+      ));
+  }
+}
