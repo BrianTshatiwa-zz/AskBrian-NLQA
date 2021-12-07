160e546673475680d00a576a55a501083c74a2b9
BrianTshatiwa <kutlwano.tshatiwa101@gmail.com>
2021-12-07 22:10:28 +0200

    create a script for question answering

---
+++ mobileApp/askbrian/lib/question_answering.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionAnswering extends StatefulWidget {
+  @override
+  _QuestionAnsweringState createState() => _QuestionAnsweringState();
+}
+
+class _QuestionAnsweringState extends State<QuestionAnswering> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
